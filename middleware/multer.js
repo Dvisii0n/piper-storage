3e3be45aa63c85f9c3fbc7e4bc0c3463a30b8902
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-17 20:04:13 -0600

    fixed req.files returning empty array

--- middleware/multer.js
+++ middleware/multer.js
@@ -17,15 +17,14 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage }).array("uploadedFiles", LIMIT);
 
-async function uploadFiles(req, res, next) {
-	if (!req.isAuthenticated()) return res.redirect("/login");
+async function multerUpload(req, res, next) {
 	upload(req, res, (err) => {
 		if (err instanceof MulterError) {
 			res.status(500).send("An error ocurred when uploading");
 		} else if (err) {
 			next(err);
 		}
+		next();
 	});
-	next();
 }
-export { uploadFiles };
+export { multerUpload };
