8278d45578fcc3be8e92ea6a613da3e8fcb80cbf
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-17 20:05:17 -0600

    added crud folder routes

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import homeController from "../controllers/homeController.js";
-import { uploadFiles } from "../middleware/multer.js";
+import { multerUpload } from "../middleware/multer.js";
+import { checkAuth } from "../middleware/authHandler.js";
 
 const homeRouter = new Router();
 
-homeRouter.get("/", homeController.getHome);
+homeRouter.use(checkAuth);
 
-homeRouter.post("/upload", uploadFiles, homeController.uploadFiles);
+homeRouter.get("/", homeController.getHome);
+homeRouter.get("/folder/:id", homeController.getFolder);
+
+homeRouter.post("/upload/:parentId", multerUpload, homeController.fileUpload);
+
+homeRouter.post("/createFolder/:parentId", homeController.createFolder);
+
+homeRouter.post("/editFolder/:id", homeController.editFolder);
+
+homeRouter.post("/deleteFolder/:id", homeController.deleteFolder);
 
 export default homeRouter;
