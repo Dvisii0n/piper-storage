198cf07524611181860745cef4c6ec6905a38daf
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-23 19:48:12 -0600

    added more validation to url params, queries and form bodies

---
+++ validation/fileValidation.js
@@ -0,0 +1,29 @@
+import { body, param, query } from "express-validator";
+import { lengthValidator } from "../utils/validationUtils.js";
+
+const validateParentId = [param("parentId").exists().escape().isInt().toInt()];
+const validateQueryFileId = [query("fileId").exists().escape().isInt().toInt()];
+
+const validateFileUpload = [...validateParentId];
+
+const validateDownloadFile = [...validateQueryFileId];
+
+const validateDeleteFile = [...validateQueryFileId];
+
+const validateEditFile = [
+	...validateQueryFileId,
+	body("newFileName")
+		.notEmpty()
+		.escape()
+		.isAlphanumeric("en-US", { ignore: "_- " })
+		.withMessage("New file name must me alphanumeric"),
+
+	lengthValidator("newFileName", { min: 1, max: 255 }),
+];
+
+export {
+	validateFileUpload,
+	validateDownloadFile,
+	validateDeleteFile,
+	validateEditFile,
+};
---
+++ validation/folderValidation.js
@@ -0,0 +1,42 @@
+import { body, param } from "express-validator";
+import { lengthValidator } from "../utils/validationUtils.js";
+
+const validateFolderName = [
+	body("folderName")
+		.notEmpty()
+		.escape()
+		.isAlphanumeric("en-US", { ignore: "_- " })
+		.withMessage("Folder name can only contain letters and numbers"),
+
+	lengthValidator("folderName", { min: 1, max: 255 }),
+];
+
+const validateNewFolderName = [
+	body("newFolderName")
+		.notEmpty()
+		.escape()
+		.isAlphanumeric("en-US", { ignore: "_- " })
+		.withMessage("New folder name can only contain letters and numbers"),
+
+	lengthValidator("newFolderName", { min: 1, max: 255 }),
+];
+
+const validateFolderId = [param("id").exists().escape().isInt().toInt()];
+
+const validateParentId = [param("parentId").exists().escape().isInt().toInt()];
+
+const validateGetFolder = [...validateFolderId];
+
+const validateCreateFolder = [...validateParentId, ...validateFolderName];
+
+const validateEditFolder = [validateFolderId, ...validateNewFolderName];
+
+const validateDeleteFolder = [...validateFolderId];
+
+export {
+	validateGetFolder,
+	validateFolderName,
+	validateCreateFolder,
+	validateEditFolder,
+	validateDeleteFolder,
+};
