407bde278b24c4ee3ee97029338c65daaa7eddff
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-31 15:01:32 -0600

    fixed history.back messing up path navigation

--- public/scripts/userNavigation.js
+++ public/scripts/userNavigation.js
@@ -1,4 +1,10 @@
 import { appendToHistory, renderPath } from "./navigation.js";
 
+window.addEventListener("pageshow", (e) => {
+	if (e.persisted) {
+		window.location.reload();
+	}
+});
+
 appendToHistory();
 renderPath();
